fc9573250aab5284bb64c417e54134c098b0d91b
Vyom Pant <vypant@deloitte.com>
2022-04-04 10:51:17 +0530

    Framework Design , added testng.xml

--- RestAssuredMainAssignment/src/test/java/Base.java
+++ RestAssuredMainAssignment/src/test/java/Base.java
@@ -19,10 +19,10 @@ import static io.restassured.RestAssured.given;
 public class Base {
 
 
-    private String username;
-    private String email;
-    private String password;
-    public String tokenGenerated;
+    public String username;
+    public String email;
+    public String password;
+    public  static String tokenGenerated;
 
     @BeforeMethod
     public void registerUser() throws IOException {
@@ -44,7 +44,7 @@ public class Base {
     }
 
     @Test(priority = 1)
-    public void authenticationTest() throws IOException {
+    public void AuthenticationTest() throws IOException {
         RestAssured.baseURI = "https://api-nodejs-todolist.herokuapp.com";
         RequestSpecification request = RestAssured.given();
         String payload = "{\n" +
@@ -68,123 +68,123 @@ public class Base {
         responseLogin.prettyPrint();
     }
 
-    @Test(priority = 2)
-    public void addTask() throws IOException {
-        RestAssured.baseURI = "https://api-nodejs-todolist.herokuapp.com/task";
-        RequestSpecification request = RestAssured.given();
-        request.header("Authorization", "Bearer " + tokenGenerated)
-                .header("Content-Type", "application/json");
-        FileInputStream inputStream = new FileInputStream("C:\\HUAPI\\RestAssuredMainAssignment\\src\\test\\data\\Add20Task.xlsx");
-        XSSFWorkbook wb = new XSSFWorkbook(inputStream);
-        XSSFSheet sheet = wb.getSheet("task20");
-        int rows = sheet.getPhysicalNumberOfRows();
-        int cols = sheet.getRow(0).getLastCellNum();
-        String description = null;
-        String task = null;
-        for (int i = 1; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (j == 0) {
-                    description = sheet.getRow(i).getCell(j).getStringCellValue();
-                }try {
-                    if (j == 1) {
-                        task = sheet.getRow(i).getCell(j).getStringCellValue();
-                    }
-                }catch (Exception e){
-                    System.out.println("Invalid Task is entered");
-                    continue;
-                }
-
-            }
-            String addTaskJson = "{\n" +
-                    "\t\""+description+"\": \""+task+"\"\n" +
-                    "}";
-            Response responseaddTask = request.body(addTaskJson).post();
-            String jsonString1 = responseaddTask.getBody().asString();
-            String task1 = JsonPath.from(jsonString1).get("data.description");
-            System.out.println(task1);
-            if (task.equals(task1)){
-                System.out.println("Task is validated");
-            }
-            else{
-                System.out.println("Invalid task");
-            }
-            responseaddTask.prettyPrint();
-            wb.close();
-            inputStream.close();
-        }
-    }
-    @Test(priority = 3)
-    public void validateUser(){
-        RestAssured.baseURI = "https://api-nodejs-todolist.herokuapp.com/user/me";
-        RequestSpecification request = RestAssured.given();
-        request.header("Authorization","Bearer "+ tokenGenerated)
-                .header("Content-Type","application/json");
-        Response responsevalidateUser = request.get();
-        responsevalidateUser.prettyPrint();
-    }
-    @Test(priority = 4)
-    public void getTask(){
-        RestAssured.baseURI = "https://api-nodejs-todolist.herokuapp.com/task";
-        RequestSpecification request = RestAssured.given();
-        request.header("Authorization","Bearer "+ tokenGenerated)
-                .header("Content-Type","application/json");
-        Response responsegetTask = request.get();
-        responsegetTask.prettyPrint();
-    }
-    @Test(priority = 5)
-    public void paginationFor2(){
-        RestAssured.baseURI = "https://api-nodejs-todolist.herokuapp.com";
-        RequestSpecification request2 = RestAssured.given();
-        request2.header("Authorization","Bearer "+ tokenGenerated)
-                .header("Content-Type","application/json");
-        Response response2 = request2.get("/task?limit=2");
-        response2.prettyPrint();
-    }
-    @Test(priority = 6)
-    public void paginationFor5(){
-        RestAssured.baseURI = "https://api-nodejs-todolist.herokuapp.com";
-        RequestSpecification request5 = RestAssured.given();
-        request5.header("Authorization","Bearer "+ tokenGenerated)
-                .header("Content-Type","application/json");
-        Response response5 = request5.get("/task?limit=5");
-        response5.prettyPrint();
-    }
-    @Test(priority = 7)
-    public void paginationFor10(){
-        RestAssured.baseURI = "https://api-nodejs-todolist.herokuapp.com";
-        RequestSpecification request10 = RestAssured.given();
-        request10.header("Authorization","Bearer "+ tokenGenerated)
-                .header("Content-Type","application/json");
-        Response response10 = request10.get("/task?limit=10");
-        response10.prettyPrint();
-    }
-    @Test(priority = 8)
-    public void InvalidAuthenticationTest() throws IOException {
-        RestAssured.baseURI = "https://api-nodejs-todolist.herokuapp.com";
-        RequestSpecification request = RestAssured.given();
-        String payload = "{\n" +
-                "  \"name\" : \""+username+"\",\n" +
-                "  \"email\" : \""+email+"\",\n" +
-                "  \"password\" : \""+password+"\"\n" +
-                "}";
-        request.header("Content-Type", "application/json");
-        Response responsefromGeneratedToken = request.body(payload).post("/user/register");
-        responsefromGeneratedToken.prettyPrint();
-        String jsonString = responsefromGeneratedToken.getBody().asString();
-        tokenGenerated = JsonPath.from(jsonString).get("token");
-        request.header("Authorization", "Bearer" + tokenGenerated)
-                .header("Content-Type", "application/json");
-        String loginDetails = "{\n" +
-                "  \"email\" : \""+email+"\",\n" +
-                "  \"password\" : \""+password+"\"\n" +
-                "}";
-        try {
-            Response responseLogin = request.body(loginDetails).post("/user/login");
-        }catch(Exception e){
-            System.out.println(e);
-        }
-
-    }
+//    @Test(priority = 2)
+//    public void addTask() throws IOException {
+//        RestAssured.baseURI = "https://api-nodejs-todolist.herokuapp.com/task";
+//        RequestSpecification request = RestAssured.given();
+//        request.header("Authorization", "Bearer " + tokenGenerated)
+//                .header("Content-Type", "application/json");
+//        FileInputStream inputStream = new FileInputStream("C:\\HUAPI\\RestAssuredMainAssignment\\src\\test\\data\\Add20Task.xlsx");
+//        XSSFWorkbook wb = new XSSFWorkbook(inputStream);
+//        XSSFSheet sheet = wb.getSheet("task20");
+//        int rows = sheet.getPhysicalNumberOfRows();
+//        int cols = sheet.getRow(0).getLastCellNum();
+//        String description = null;
+//        String task = null;
+//        for (int i = 1; i < rows; i++) {
+//            for (int j = 0; j < cols; j++) {
+//                if (j == 0) {
+//                    description = sheet.getRow(i).getCell(j).getStringCellValue();
+//                }try {
+//                    if (j == 1) {
+//                        task = sheet.getRow(i).getCell(j).getStringCellValue();
+//                    }
+//                }catch (Exception e){
+//                    System.out.println("Invalid Task is entered");
+//                    continue;
+//                }
+//
+//            }
+//            String addTaskJson = "{\n" +
+//                    "\t\""+description+"\": \""+task+"\"\n" +
+//                    "}";
+//            Response responseaddTask = request.body(addTaskJson).post();
+//            String jsonString1 = responseaddTask.getBody().asString();
+//            String task1 = JsonPath.from(jsonString1).get("data.description");
+//            System.out.println(task1);
+//            if (task.equals(task1)){
+//                System.out.println("Task is validated");
+//            }
+//            else{
+//                System.out.println("Invalid task");
+//            }
+//            responseaddTask.prettyPrint();
+//            wb.close();
+//            inputStream.close();
+//        }
+//    }
+//    @Test(priority = 3)
+//    public void validateUser(){
+//        RestAssured.baseURI = "https://api-nodejs-todolist.herokuapp.com/user/me";
+//        RequestSpecification request = RestAssured.given();
+//        request.header("Authorization","Bearer "+ tokenGenerated)
+//                .header("Content-Type","application/json");
+//        Response responsevalidateUser = request.get();
+//        responsevalidateUser.prettyPrint();
+//    }
+//    @Test(priority = 4)
+//    public void getTask(){
+//        RestAssured.baseURI = "https://api-nodejs-todolist.herokuapp.com/task";
+//        RequestSpecification request = RestAssured.given();
+//        request.header("Authorization","Bearer "+ tokenGenerated)
+//                .header("Content-Type","application/json");
+//        Response responsegetTask = request.get();
+//        responsegetTask.prettyPrint();
+//    }
+//    @Test(priority = 5)
+//    public void paginationFor2(){
+//        RestAssured.baseURI = "https://api-nodejs-todolist.herokuapp.com";
+//        RequestSpecification request2 = RestAssured.given();
+//        request2.header("Authorization","Bearer "+ tokenGenerated)
+//                .header("Content-Type","application/json");
+//        Response response2 = request2.get("/task?limit=2");
+//        response2.prettyPrint();
+//    }
+//    @Test(priority = 6)
+//    public void paginationFor5(){
+//        RestAssured.baseURI = "https://api-nodejs-todolist.herokuapp.com";
+//        RequestSpecification request5 = RestAssured.given();
+//        request5.header("Authorization","Bearer "+ tokenGenerated)
+//                .header("Content-Type","application/json");
+//        Response response5 = request5.get("/task?limit=5");
+//        response5.prettyPrint();
+//    }
+//    @Test(priority = 7)
+//    public void paginationFor10(){
+//        RestAssured.baseURI = "https://api-nodejs-todolist.herokuapp.com";
+//        RequestSpecification request10 = RestAssured.given();
+//        request10.header("Authorization","Bearer "+ tokenGenerated)
+//                .header("Content-Type","application/json");
+//        Response response10 = request10.get("/task?limit=10");
+//        response10.prettyPrint();
+//    }
+//    @Test(priority = 8)
+//    public void InvalidAuthenticationTest() throws IOException {
+//        RestAssured.baseURI = "https://api-nodejs-todolist.herokuapp.com";
+//        RequestSpecification request = RestAssured.given();
+//        String payload = "{\n" +
+//                "  \"name\" : \""+username+"\",\n" +
+//                "  \"email\" : \""+email+"\",\n" +
+//                "  \"password\" : \""+password+"\"\n" +
+//                "}";
+//        request.header("Content-Type", "application/json");
+//        Response responsefromGeneratedToken = request.body(payload).post("/user/register");
+//        responsefromGeneratedToken.prettyPrint();
+//        String jsonString = responsefromGeneratedToken.getBody().asString();
+//        tokenGenerated = JsonPath.from(jsonString).get("token");
+//        request.header("Authorization", "Bearer" + tokenGenerated)
+//                .header("Content-Type", "application/json");
+//        String loginDetails = "{\n" +
+//                "  \"email\" : \""+email+"\",\n" +
+//                "  \"password\" : \""+password+"\"\n" +
+//                "}";
+//        try {
+//            Response responseLogin = request.body(loginDetails).post("/user/login");
+//        }catch(Exception e){
+//            System.out.println(e);
+//        }
+//
+//    }
 
 
 
